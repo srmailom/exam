e1f43c544a0ea63b5f4af25054f19ed24ef5e8ec
DESKTOP-LEAEJVQ\whin <whinmailom@gmail.com>
2025-01-09 02:07:51 +0800

    activity6

--- src/app/exam/activity6/page.tsx
+++ src/app/exam/activity6/page.tsx
@@ -1,6 +1,49 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+/* eslint-disable @next/next/no-img-element */
+'use client'
+
+import axios from "axios"
+import { useState, useEffect } from "react"
+
 const Activity6 = () => {
+ const [data, setData] = useState<any[]>([]);  
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await axios.get('https://random-data-api.com/api/v3/projects/4cd78355-9cc7-4749-aab5-5e8f64271cc8?api_key=hFD4QdeTGuZ_sahREesstA', {
+          headers: {
+            "Content-Type": "application/json",
+          },
+        });
+
+        setData(response.data.data);
+      } catch (error) {
+        console.error("Error:", error);
+      }
+    };
+
+    fetchData();
+  }, []);
+
   return (
-    <div>Activity6</div>
+    <div className="p-4">
+      <h1 className="text-2xl font-semibold mb-4">User Cards</h1>
+      <div className="flex flex-wrap justify-center gap-4">
+        {data.map((user, index) => (
+          <div key={index} className="max-w-xs w-full bg-white shadow-lg rounded-lg overflow-hidden">
+            <img 
+              src={user.avatar} 
+              alt={user.first_name} 
+              className="w-full h-48 object-cover"
+            />
+            <div className="p-4">
+              <h2 className="text-lg font-semibold">{user.first_name} {user.last_name}</h2>
+              <p className="text-sm text-gray-600 mt-2">{user.description}</p>
+            </div>
+          </div>
+        ))}
+      </div>
+    </div>
   )
 }
 
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,44 +1,11 @@
 /* eslint-disable @typescript-eslint/no-unused-vars */
-'use client'
-import React, { useEffect, useState } from "react";
 
 export default function Home() {
-  const [size, setSize] = useState(60)
-  const [color, setColor] = useState('#D1D5DB')
-  
-  const generateRandomColor = () => {
-    const hexCharacter = '0123456789ABCDEF'
-
-    let color = '#'
-
-    for (let i = 0; i < 6 ; i++){
-      color += hexCharacter[Math.floor(Math.random() * 16)]
-    }
-    return color
-  }
-
-  const handleGrow = () => {
-    setSize(size * 2)
-    setColor(generateRandomColor())
-    console.log(color)
-    console.log(size)
-  }
-
   return (
-
-    //Activity 4
-    <div className="flex items-center justify-center h-screen">
-      <button
-      style={{
-          backgroundColor: color, 
-          width: `${size}px`,
-          height: `${size}px`,
-          fontSize: '14px',
-          transition: 'all 0.3s ease'
-        }} 
-        onClick={handleGrow}>
-       GROW
-      </button>
+    <div>
+      <p>Hello!</p>
+      <p>Please access this link http://13.229.228.3:3000/exam/activity1 to check the technical exam </p>
+      <p>note: change only the activity number at the end of the link. Thank you!</p>
     </div>
   );
 }
