440fce6597cf6fc912b69ef3ae7d528a491b8d5a
srmailom <150770828+srmailom@users.noreply.github.com>
2025-01-08 18:54:26 +0800

    Update page.tsx

--- src/app/exam/activity3/page.tsx
+++ src/app/exam/activity3/page.tsx
@@ -1,4 +1,4 @@
-// eslint-disable-next-line @typescript-eslint/no-unused-vars
+/* eslint-disable @typescript-eslint/no-explicit-any */
 'use client'
 
 import React, { useEffect, useState } from 'react'
